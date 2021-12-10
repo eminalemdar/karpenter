a209bff9cab5b676facc12e5ebef9b5040b09698
eminalemdar <emin@kloia.com>
2021-12-10 19:15:09 +0300

    modified iam

--- terraform-eks/iam.tf
+++ terraform-eks/iam.tf
@@ -12,6 +12,37 @@ resource "aws_iam_instance_profile" "karpenter" {
   role = module.eks.worker_iam_role_name
 }
 
+resource "aws_iam_policy" "karpenter_controller_policy" {
+  name        = "karpenter_controller_policy"
+  path        = "/"
+  description = "Karpenter Controller Policy"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:CreateLaunchTemplate",
+          "ec2:CreateFleet",
+          "ec2:RunInstances",
+          "ec2:CreateTags",
+          "iam:PassRole",
+          "ec2:TerminateInstances",
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeInstanceTypeOfferings",
+          "ec2:DescribeAvailabilityZones",
+          "ssm:GetParameter"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
 module "iam_assumable_role_karpenter" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version                       = "4.7.0"
